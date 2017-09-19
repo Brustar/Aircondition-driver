bb3ac176454776723553e498539ba69ff5cdd9a4
Brustar <yeals2@gmail.com>
2017-09-19 09:59:50 +0800

    fb

--- Freshair.lua
+++ Freshair.lua
@@ -1,44 +1,64 @@
 Freshair = {}
 
-CONTROL_NUMBER = "01"
---CONTROL_READ = "03"
 CONTROL_READ = "01 03 9c f9 00 08"
-CONTROL_WRITE = "10"
-CONTROL_ADDR = "01 10 9D 06 00 08 10"
+--CONTROL_ADDR = "01 10 9D 06 00 08 10"
+
+--CONTROL_COOL = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_WIND = "00 20 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_HOT = "00 40 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
 --CONTROL_ADDR2 = "9D 06 00 08 10"
-CONTROL_JCQ = "00 08"
-CONTROL_LENGTH = "10"
 
---CONTROL_HOT = "00 40"
---CONTROL_COOL = "00 08"
-CONTROL_COOL = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_WIND = "00 20 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_HOT = "00 40 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_ON = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_OFF = "00 08 00 02 00 00 00 15 00 00 00 05 00 00 00 00"
 
-CONTROL_TEMPELATE = "00 15"
-
-
-CONTROL_ON = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_OFF = "00 08 00 02 00 00 00 15 00 00 00 05 00 00 00 00"
-CONTROL_ON1 = "00 20"
-CONTROL_OFF1 = "00 00"
-
-CONTROL_EMPTY = "00 00"
-
-CONTROL_HIGH = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_MIDDLE = "00 08 00 24 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_LOW = "00 08 00 28 00 00 00 15 00 00 00 05 00 00 00 20"
-CONTROL_AUTO = "00 08 00 21 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_HIGH = "00 08 00 22 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_MIDDLE = "00 08 00 24 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_LOW = "00 08 00 28 00 00 00 15 00 00 00 05 00 00 00 20"
+--CONTROL_AUTO = "00 08 00 21 00 00 00 15 00 00 00 05 00 00 00 20"
 CONTROL_STOP = "00 08 00 02 00 00 00 15 00 00 00 05 00 00 00 00"
+
+CONTROL_NUMBER = "01"	--设备号
+--CONTROL_READ = "03"		--读
+CONTROL_WRITE = "10"	--写
+
+CONTROL_WRITE_ADDR = "9D 06"	--写地址
+CONTROL_READ_ADDR = "9C F9"	--读地址
+CONTROL_REGISTER = "00 08"	--寄存器个数
+CONTROL_LENGTH = "10"	--长度
+
+CONTROL_HOT = "00 40"	--制热
+CONTROL_COOL = "00 08"	--制冷
+CONTROL_WIND = "00 20"	--送风
+
+
+CONTROL_TEMPELATE = "00 15"	--温度
+
+
+CONTROL_ON = "00 20"	--开
+CONTROL_OFF = "00 00"	--关
+
+CONTROL_EMPTY = "00 00"	--空
+
+CONTROL_HIGH = "00 22"	--高风
+CONTROL_MIDDLE = "00 24"	--中风
+CONTROL_LOW = "00 28"	--低风
+CONTROL_AUTO = "00 21" --自动
+
+
 
 
 function Freshair:create()
     local freshair = {}
 
     function freshair:createCMD(action)
-    	--local cmd = CONTROL_ADDR .. " " .. CONTROL_WRITE .." 00 " .. action
-    
-	local cmd = CONTROL_ADDR .. " "..action
+    local cmd = CONTROL_NUMBER ..' '.. CONTROL_WRITE ..' '.. CONTROL_WRITE_ADDR ..' ' .. CONTROL_REGISTER .. ' ' .. CONTROL_LENGTH
+    if (action==CONTROL_OFF) then
+	   cmd = cmd .. " " ..CONTROL_COOL .. " " .. "00 02" .. " " .. CONTROL_EMPTY .. " " .. CONTROL_TEMPELATE .. " " .. "00 00 00 05 00 00" .. CONTROL_OFF 
+    elseif (action==CONTROL_WIND) then
+	   cmd = cmd .. " " .. CONTROL_WIND .. " " .. CONTROL_AUTO .. " " .. CONTROL_EMPTY .. " " .. CONTROL_TEMPELATE .. " " .. "00 00 00 05 00 00" .. CONTROL_ON
+    else
+	   cmd = cmd .. " " ..CONTROL_COOL .. " " .. action .. " " .. CONTROL_EMPTY .. " " .. CONTROL_TEMPELATE .. " " .. "00 00 00 05 00 00" .. CONTROL_ON 
+    end 
 	local pack = Pack:create()
 	cmd = cmd .. " " .. pack.crc16(tohex(cmd))
 
@@ -71,10 +91,11 @@ function Freshair:create()
     end
     
     function freshair:READ()
-        local cmd = CONTROL_READ
-	   local pack = Pack:create()
-	   cmd = cmd .. " " .. pack.crc16(tohex(cmd))
-	   return cmd
+    	 local cmd = CONTROL_READ
+	 local pack = Pack:create()
+	 cmd = cmd .. " " .. pack.crc16(tohex(cmd))
+
+	 return cmd
     end
     
     function freshair:WIND()
--- Pack.lua
+++ Pack.lua
@@ -60,6 +60,13 @@ function Pack:create()
     	return string.pack(pattern,VI_HEAD,cmd,3,deviceID,state,checksum)
     end
     
+    --01 03 10 00 09 00 24 00 08 00 15 00 22 00 05 00 00 00 20
+    function pack.decodeFresh(data)
+	   local pattern = "bbbbbbbbbbbbbbbbbbb"
+	   local _,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,power= string.unpack(data,pattern)
+	   C4:SetVariable("FRESH_POWER", power==0x20)  
+    end
+    
     function pack.decodeAirFB(data)
 	   local pattern = "bbbbbbbbbbbbbbb"
 	   local _,_,_,_,_,_,_,power,_,mode,speed,temp = string.unpack(data,pattern)
